d5c8ac70d525158065db6b249235b25d2396a450
bazrafkan <bazrafkan.mb@gmail.com>
2020-04-29 22:13:58 +0200

    feat: update joke task to get joke from backend

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeTask.java
@@ -2,9 +2,17 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.AsyncTask;
 
-import com.udacity.gradle.javajokes.Jokes;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 public class JokeTask extends AsyncTask<Void, Void, String> {
+    private static MyApi apiService = null;
+
     public interface AsyncJokeTaskResult {
         void onPreExecute();
 
@@ -25,7 +33,27 @@ public class JokeTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... voids) {
-        return Jokes.getRandomJoke();
+        if(apiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            apiService = builder.build();
+        }
+
+        try {
+            return apiService.getJoke().execute().getJoke();
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     @Override
